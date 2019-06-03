6819babda394a0f07ebfb35d7f3c7d1e435e67b4
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2019-06-03 20:30:10 +0200

    Updated routes file

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,17 @@
 
 Route::get('/', 'ListingController@main')->name('index');
 
-Route::get('/listing/{id}', 'ListingController@view')->name('view');
-
 Route::get('/listings', 'ListingController@index')->name('listing.index');
 
-Route::any('/search', 'ListingController@search')->name('listing.search');
-
 Route::get('/listing/create', 'ListingController@create')->name('listing.create');
+
+Route::any('/search', 'ListingController@search')->name('listing.search');
 
 Route::get('/listing/edit/{id}', 'ListingController@edit')->name('listing.edit');
 
 Route::get('/listing/show/{id}', 'ListingController@show')->name('listing.show');
+
+Route::get('/listing/{id}', 'ListingController@view')->name('view');
 
 Auth::routes();
 
