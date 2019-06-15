9b2d4ed7fae58a88c1d18dc437694ec1b36c1844
Aleksandar <43018917+Axon98@users.noreply.github.com>
2019-06-15 18:28:26 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                     {{ config('app.name', 'BigDeal') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <a id="nav-toggle" href="#"><span></span></a>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
